05cba4ec8d096bb3aa4cb2db9e8921b3811830b1
Kurt <kstyers86@gmail.com>
2021-10-17 08:06:19 -0700

    Add function to pick sidearm or longarm
    Added a function that will randomly choose a sidearm or a longarm but no more than 2 per loadout. Also added a while loop that will keep picking a new longarm if it is a duplicate.

--- script.js
+++ script.js
@@ -5,9 +5,43 @@ const getRandom = (max) => {
     return Math.ceil(Math.random() * max);
 }
 
+const getEquipSlots = (numEquip) => {
+    let equipSlots = ["sidearm"];
+
+    if (numEquip != 1) {
+        let sidearms = 1;
+        let longarms = 0;
+        let type = 0;
+
+        for (let i = 0; i < numEquip; i++) {
+           type = getRandom(2);
+
+           if (type === 1) {
+               if (sidearms === 2) {
+                   equipSlots.push("longarm")
+                   longarms += 1;
+               } else {
+                   equipSlots.push("sidearm");
+                   sidearms += 1;
+               }            
+           }
+           if (type === 2) {
+                if (longarms === 2) {
+                    equipSlots.push("sidearm")
+                    sidearms += 1;
+                } else {
+                    equipSlots.push("longarm");
+                    longarms += 1;
+                }
+            }
+        }
+    }
+    return equipSlots;
+}
+
 const getWeaponType = (equipSlot) => {
-    let sidearms = ["revolvers", "pistols", "sawed-off"];
-    let longarms = ["repeaters", "rifles", "shotguns", "bows"];
+    const sidearms = ["revolvers", "pistols", "sawed-off"];
+    const longarms = ["repeaters", "rifles", "shotguns", "bows"];
 
     if (equipSlot === "sidearm") {
         return sidearms[getRandom(sidearms.length) - 1];
@@ -25,25 +59,23 @@ const getWeapon = (weaponType) => {
 
 const buildLoadout = () => {
     const numEquip = getRandom(weapons['MAX WEAPONS']);
+    let equipSlots = getEquipSlots(numEquip);
     let equipment = [];
+    let weapon = "";
 
-    if (numEquip === 1) {
-        equipment.push(getWeapon(getWeaponType("sidearm")));
-    } else if (numEquip === 2) {
-        equipment.push(getWeapon(getWeaponType("sidearm")));
-        equipment.push(getWeapon(getWeaponType("longarm")));
-    } else if (numEquip === 3) {
-        equipment.push(getWeapon(getWeaponType("sidearm")));
-        equipment.push(getWeapon(getWeaponType("longarm")));
-        equipment.push(getWeapon(getWeaponType("sidearm")));
-    } else if (numEquip === 4) {
-        equipment.push(getWeapon(getWeaponType("sidearm")));
-        equipment.push(getWeapon(getWeaponType("longarm")));
-        equipment.push(getWeapon(getWeaponType("sidearm")));
-        equipment.push(getWeapon(getWeaponType("longarm")));
-    } else {
-        return "Error - Number of MAX WEAPONS invalid";
+    for (let i = 0; i < numEquip; i++) {
+        weapon = getWeapon(getWeaponType(equipSlots[i]))
+
+        // Prevent duplicates of longarm weapons
+        while (equipSlots[i] === "longarm" && equipment.some(element => element === weapon)) {
+            weapon = getWeapon(getWeaponType(equipSlots[i]))
+        }
+
+        equipment.push(weapon);
     }
+    
+    equipment.push(getWeapon("thrown"));
+    equipment.push(getWeapon("melee"));
 
     return equipment;
 }
